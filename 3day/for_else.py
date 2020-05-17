d72a804a20a3d3db62eb641956bbe52544b3b9c0
xdx <16348104@qq.com>
2020-05-17 17:44:28 +0800

    test

--- 3day/for_else.py
+++ 3day/for_else.py
@@ -1,9 +1,9 @@
-# for i in 'Python':
-#     if i =="y":
-#         continue
-# ##for循环正常结束后执行else语句
-#     print(i)
-# else:
-#     print('for循环正常结束后执行else语句')
+for i in 'Python':
+    if i == "y":
+        continue
+##for循环正常结束后执行else语句
+    print(i)
+else:
+    print('for循环正常结束后执行else语句')
 
-# print('end')
+print('end')
